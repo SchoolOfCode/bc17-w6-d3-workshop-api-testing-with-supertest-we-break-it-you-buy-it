069785ed83d1e28c6a8bbcccd5d6f79b0689fafa
Ryan <ryanshaunsmith@gmail.com>
2024-08-07 16:56:04 +0100

    mostly completed ticket 6 still need to break it

--- users/users.test.js
+++ users/users.test.js
@@ -25,39 +25,30 @@ test("GET /api/health works", async () => {
 test("GET /api/users works", async () => {
   // ARRANGE
   await resetUsersTable(seedData);
-  
+
   // ACT
   const response = await request(app).get("/api/users/");
 
-  const resBody = response.body
-  const resBodySuccess = response.body.success
+  const resBody = response.body;
+  const resBodySuccess = response.body.success;
   const statusCode = 200;
-  // check response body
-  // expect(response.body).toEqual(resBody);
-  // check status code
+  // assert that the response body is an object
   expect(resBody).toBeTypeOf("object");
+  // check status code
   expect(response.status).toEqual(statusCode);
+  // assert that response body.payload is an array
   expect(resBody.payload).toBeTypeOf("object");
-  expect(resBodySuccess).toBe(true);
+  // loop over the payload array. for each user object in the payload array:
+  resBody.payload.forEach((arrItem) => {
+    // assert that user object.id is a number
+    expect(arrItem.id).toBeTypeOf("number");
+    // assert that user object.username is a string
+    expect(arrItem.username).toBeTypeOf("string");
+  });
+  // assert that response body.success is true
+  expect(resBodySuccess).toBeTruthy();
   // check response headers
   expect(response.headers["content-type"]).toMatch("application/json");
 });
-// run tests to make sure the skeleton passes on its own
-// then within the test:
-//    ARRANGE:
-//      use the `resetUsersTable` function to reset the database table to a known state
-//    ACT:
-//      use Supertest to send a GET request to the `/api/users` endpoint
-//      wait for the response
-//    ASSERT:
-//      assert that the response body is an object
-//      assert that response body.success is true
-//      assert that response body.payload is an array
-//      loop over the payload array. for each user object in the payload array:
-//          assert that user object.id is a number
-//          assert that user object.username is a string
-//      assert that the response status code is 200
-//      assert that there's a Content-Type response header which contains `application/json`
-//      any other assertions that you think would be useful
 // run tests to ensure they passes
 // temporarily break the implementation in `users/users.controller.js` to ensure test fails and then change back so that tests pass
